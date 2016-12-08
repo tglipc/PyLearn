05fe8a36fd0744f7e7576b71a6a5cc24209e82d2
IPC Students <info@ipc39.ru>
2016-12-08 11:39:31 +0200

    Walk function

---
+++ Dirwalk.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import os
+
+rootDir = '.'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+        print('\t%s' % fname)
